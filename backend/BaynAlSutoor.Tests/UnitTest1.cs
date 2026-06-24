ebbd708847860a031370318eb20511a68d500764
Mohamed Zaky <zakym6883@gmail.com>
2026-06-24 11:04:08 +0300

    Resolve final merge conflict in tests

--- backend/BaynAlSutoor.Tests/UnitTest1.cs
+++ backend/BaynAlSutoor.Tests/UnitTest1.cs
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
 using Microsoft.Extensions.Configuration;
 using System.IO;
 using Xunit;
 
 namespace BaynAlSutoor.Tests;
-=======
-﻿namespace BaynAlSutoor.Tests;
->>>>>>> origin/main
 
 public class UnitTest1
 {
     [Fact]
-<<<<<<< HEAD
     public void TestConfig()
     {
         var builder = new ConfigurationBuilder()
@@ -23,10 +18,5 @@ public class UnitTest1
         var secretKey = jwtSettings.GetValue<string>("Secret");
         
         Assert.Equal("SuperSecretKeyForBaynAlSutoorProjectThatIsVeryLongAndSecure!", secretKey);
-=======
-    public void Test1()
-    {
-
->>>>>>> origin/main
     }
 }
